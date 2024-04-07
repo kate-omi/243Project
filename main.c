7b6c72a5520bc1c7286b9b138d10c6fc5badd31d
kate-omi <kate.ker@mail.utoronto.ca>
2024-04-06 22:52:06 -0400

    resetting to try and fix error, still incomplete

--- main.c
+++ main.c
@@ -1,24 +1,68 @@
 // Last update: 2021-05-06 20:00:00
 #include "address_map_nios2.h"
+
 #include "vga.h"
+#include "keyboard.h"
 
-#define PS2_BASE 0xff200100
-#define PIXEL_BASE 0xFF203020
 #define NUMWORDS 3
-//#include <stdlib.h>
+#define NUMLETTERS 3
 
-void drawScreen(int screen);
-void plot_pixel(int x, int y, short int line_color);
-void clear_screen();
+#define COMPUTER 1
+#define MULTIPLAYER 2
 
-char keyboard();
-void wordCompare(char word[5], int screennumber);
+#define START 0
+#define MODESELECT 1
+#define PENDING 2
+#define INCORRECT 3
+#define CORRECT 4
+
+struct Word {
+    int status;
+    char word[NUMLETTERS + 1];
+    int screen;
+};
+
+struct Game {
+    int mode;
+    int state;
+    struct Word word;
+};
+
+struct Game game;
+
+void start();
+void modeSelect();
+void incorrect();
+void correct();
 
 void playComputer();
 void playMultiplayer();
 
+void wordCompare(char word[5], int screennumber);
+void assignWord(struct Word wordSelect);
+
+
 int main() {
+    game.state = START;
     while (1) { //infinite loop
+        switch (game.state) {
+            case START:
+                start();
+                break;
+            case MODESELECT:
+                modeSelect();
+                break;
+            case PENDING:
+                wordCompare(game.word.word, game.word.screen);
+                break;
+            case INCORRECT:
+                incorrect();
+                break;
+            case CORRECT:
+                correct();
+                break;
+        }
+
         //start screen
         drawScreen(1);
         //press enter to start
@@ -41,156 +85,77 @@ int main() {
     };
 }
 
+void start() {
+    drawScreen(1);
+    while (keyboard() != '\n') {}    //waiting for user input
+    game.state = MODESELECT;
+    return;
+}
 
-char keyboard() {
-    unsigned char byte1 = 0;
-    unsigned char byte2 = 0;
-
-    volatile int * PS2_ptr = (int *)PS2_BASE;
-
-    int PS2_data;
-    int RVALID;
-
+void modeSelect() {
+    int i = 1;
     char output;
-
-    while (1) {
-        PS2_data = *(PS2_ptr); // read the Data register in the PS/2 port
-        RVALID = PS2_data & 0x8000;
-
-        if (RVALID) {
-            byte1 = byte2;
-            byte2 = PS2_data & 0xFF;
-        }
-
-        if (byte1 == 0xF0) {    //if key released
-            switch (byte2) {    //letter mapping
-                //LETTERS
-                case 0x1C:
-                    output = 'A';
-                    break;
-                case 0x32:
-                    output = 'B';
-                    break;
-                case 0x21:
-                    output = 'C';
-                    break;
-                case 0x23:
-                    output = 'D';
-                    break;
-                case 0x24:
-                    output = 'E';
-                    break;
-                case 0x2B:
-                    output = 'F';
-                    break;
-                case 0x34:
-                    output = 'G';
-                    break;
-                case 0x33:
-                    output = 'H';
-                    break;
-                case 0x43:
-                    output = 'I';
-                    break;
-                case 0x3B:
-                    output = 'J';
-                    break;
-                case 0x42:
-                    output = 'K';
-                    break;
-                case 0x4B:
-                    output = 'L';
-                    break;
-                case 0x3A:
-                    output = 'M';
-                    break;
-                case 0x31:
-                    output = 'N';
-                    break;
-                case 0x44:
-                    output = 'O';
-                    break;
-                case 0x4D:
-                    output = 'P';
-                    break;
-                case 0x15:
-                    output = 'Q';
-                    break;
-                case 0x2D:
-                    output = 'R';
-                    break;
-                case 0x1B:
-                    output = 'S';
-                    break;
-                case 0x2C:
-                    output = 'T';
-                    break;
-                case 0x3C:
-                    output = 'U';
-                    break;
-                case 0x2A:
-                    output = 'V';
-                    break;
-                case 0x1D:
-                    output = 'W';
-                    break;
-                case 0x22:
-                    output = 'X';
-                    break;
-                case 0x35:
-                    output = 'Y';
-                    break;
-                case 0x1A:
-                    output = 'Z';
-                    break;
-
-                //NUMBERS
-                case 0x45:
-                    output = '0';
-                    break;
-                case 0x16:
-                    output = '1';
-                    break;
-                case 0x1E:
-                    output = '2';
-                    break;
-
-                //ALTERNATIVE CHARACTERS
-                case 0x29:
-                    output = ' ';
-                    break;
-                case 0x5A:
-                    output = '\n';
-                    break;
-                case 0x66:
-                    output = '\b';
-                    break;
-
-                default:
-                    output = '0'; // default case if no match found
-            }       
-        return output;
-        }
+    while (i == 1) {    //wait for user to select mode
+        output = keyboard();
+        if ((output == '1') || (output == '2'))
+            i = 0;
     }
-return 0;
+
+    if (output == '1') {
+        game.mode = COMPUTER;
+        playComputer();
+    } else {
+        game.mode = MULTIPLAYER;
+        playMultiplayer();
+    }
+
+    return;
+}
+
+void incorrect() {
+    drawScreen(10);
+    //timer(1000000000) //2 seconds
+    game.state = PENDING;
+    drawScreen(game.word.screen);
+    return;
+}
+
+void correct() {
+    drawScreen(11);
+    //timer(1000000000) //2 seconds
+    game.state = START;
+    return;
 }
 
 void playComputer() {
-    char words[NUMWORDS][5] = { "CAT", "DOG", "CAR"};
+    struct Word {
+        int status;
+        char word[5];
+        int screen;
+    };
+
+    int wordPool[NUMWORDS] = {0, 1, 2};
+
+    struct Word cat = {0, "CAT", 30};
+    struct Word dog = {1, "DOG", 40};
+    struct Word car = {2, "CAR", 50};
+
     /*  OTHER POTENTIAL WORDS: Bat, Ant, Bow, Bed, Car */
  
-    int wordselect = 0;
-    int screenselect = 30;
-    char word[5];
-    
-    int i;
-    for (i = 0; words[wordselect][i] != '\0'; ++i)
-        word[i] = words[wordselect][i];
-    word[i] = '\0';
-
-    if (wordselect == 0) {
-        wordCompare(word, screenselect);    //if wordselect = cat
+    int wordselect = 0; //could make random number using rand() % NUMWORDS for more options
+    switch (wordselect) {
+        case 0:
+            assignWord(cat);
+            break;
+        case 1:
+            assignWord(dog);
+            break;
+        case 2:
+            assignWord(car);
+            break;
     }
+
+    wordCompare (game.word.word, game.word.screen);
 
     //structured so more words can be added easily
 
@@ -203,16 +168,26 @@ void playMultiplayer() {
 
 void wordCompare(char word[5], int screennumber) {
     int i = 0;
-    drawScreen(screennumber);
-    while (i != 3) {
-        if (keyboard() != word[i])
+    drawScreen(game.word.screen);
+    while (i != NUMLETTERS) {
+        if (keyboard() != game.word.word[i])
             drawScreen(10);
         else {
+            game.word.screen++;
             i++;
-            drawScreen(screennumber + i);
+            drawScreen(game.word.screen);
         }
 
     }
-    drawScreen(11);
+    game.state = CORRECT;
+    return;
+}
+
+void assignWord(struct Word wordSelect) {
+    game.word.status = wordSelect.status;
+    game.word.screen = wordSelect.screen;
+    for (int i = 0; i < NUMLETTERS; i++) {
+        game.word.word[i] = wordSelect.word[i];
+    }
     return;
 }
